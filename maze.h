8932f9219d15c718ed4e6010969283b96f78abf7
NicolasCajas1 <129789172+NicolasCajas1@users.noreply.github.com>
2023-06-12 15:29:57 -0500

    maze.h

--- maze.h
+++ maze.h
@@ -1,3 +1,3 @@
 void imprimir_laberinto();
-void es_movimiento_valido(int fila, int columna);
 int comprobar_movimiento(int fila, int columna, int* movimientos);
+int es_movimiento_valido(int x, int y);
